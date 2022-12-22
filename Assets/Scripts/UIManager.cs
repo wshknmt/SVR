e453e07ac6654d9b17821fc17b6b1c0c6f58441f
Paweł Martyniuk <wshknmt@gmail.com>
2022-12-22 20:27:57 +0100

    svr-ikea-2.0

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private GraphicRaycaster raycaster;
+    private PointerEventData pData;
+    private EventSystem eventSystem;
+
+    public Transform selectionPoint;
+    
+    public static UIManager instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<UIManager>();
+            }
+            return instance;
+        }
+        
+    }
+    
+    void Start()
+    {
+        raycaster = GetComponent<GraphicRaycaster>();
+        eventSystem = GetComponent<EventSystem>();
+        pData = new PointerEventData(eventSystem);
+        pData.position = selectionPoint.position;
+        
+    }
+
+    public bool OnEntered(GameObject button)
+    {
+        List<RaycastResult> results = new List<RaycastResult>();
+        raycaster.Raycast(pData, results);
+
+        foreach (RaycastResult result in results)
+        {
+            if (result.gameObject == button)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
